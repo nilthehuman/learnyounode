47b13d6d91873861b78c33e543fa94e642a6c458
Dániel Arató <nil.the.human@gmail.com>
2016-04-28 15:14:19 +0200

    Fix indentation

--- 07_httpclient/httpclient.js
+++ 07_httpclient/httpclient.js
@@ -3,8 +3,8 @@ const http = require('http');
 const page = process.argv[2];
 
 http.get( page, function (response) {
-  response.setEncoding('utf8');
-  response.on('data', console.log);
-  response.on('error', console.error);
+    response.setEncoding('utf8');
+    response.on('data', console.log);
+    response.on('error', console.error);
 } );
 
